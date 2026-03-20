057d649b7a575ca9f28d83e391864b099e0006ba
Sofiia <sophia.yarmak@gmail.com>
2026-03-20 19:44:03 +0100

    form finished

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -12,3 +12,34 @@ form.addEventListener('input', event => {
 
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 });
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log('Submitted Data:', formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: '', message: '' };
+  form.reset();
+});
+
+function populateForm() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedData) {
+    try {
+      const parsedData = JSON.parse(savedData);
+      formData = parsedData;
+
+      form.elements.email.value = formData.email ?? '';
+      form.elements.message.value = formData.message ?? '';
+    } catch (error) {
+      console.error('Error parsing local storage data:', error);
+    }
+  }
+}
