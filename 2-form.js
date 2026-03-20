4738172a96e53cbef4a93ec248ab6a9e2949d8fe
sofiiayarmak <sophia.yarmak@gmail.com>
2026-03-20 18:08:34 +0000

    Deploying to gh-pages from @ sofiiayarmak/goit-advancedjs-hw-01@6f37b5a55b6b0824e2ec2ef628ca2dd1a708acde 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */let e={email:"",message:""};const r=document.querySelector(".feedback-form"),m="feedback-form-state";populateForm();r.addEventListener("input",t=>{const{name:o,value:a}=t.target;e[o]=a.trim(),localStorage.setItem(m,JSON.stringify(e))});
+//# sourceMappingURL=2-form.js.map
